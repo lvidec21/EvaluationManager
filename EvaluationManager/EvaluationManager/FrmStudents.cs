ed47d7708e43c81ad4e5a907e617ed9a4f8a0de8
Luka Videc <lvidec21@foi.hr>
2023-05-13 18:22:29 +0200

    Implementiran prikaz liste studenata

--- EvaluationManager/EvaluationManager/FrmStudents.cs
+++ EvaluationManager/EvaluationManager/FrmStudents.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EvaluationManager.Models;
+using EvaluationManager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,21 @@ namespace EvaluationManager {
     public partial class FrmStudents : Form {
         public FrmStudents() {
             InitializeComponent();
+        }
+
+        private void FrmStudents_Load(object sender, EventArgs e) 
+        {
+            ShowStudents();
+        }
+        private void ShowStudents() 
+        {
+            List<Student> students = StudentRepository.GetStudents();
+            dgvStudents.DataSource = students;
+
+            dgvStudents.Columns["Id"].DisplayIndex= 0;
+            dgvStudents.Columns["FirstName"].DisplayIndex = 1;
+            dgvStudents.Columns["LastName"].DisplayIndex = 2;
+            dgvStudents.Columns["Grade"].DisplayIndex = 3;
         }
     }
 }
