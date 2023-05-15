b91dbe839ae8ac8d6853e8b21866148fc0a5d38d
Luka Videc <lvidec21@foi.hr>
2023-05-15 17:24:37 +0200

    Forma za evaluacije povezana s repozitorijem evaluacija

--- EvaluationManager/EvaluationManager/FrmEvaluation.cs
+++ EvaluationManager/EvaluationManager/FrmEvaluation.cs
@@ -14,6 +14,9 @@ namespace EvaluationManager {
     public partial class FrmEvaluation : Form 
     {
         private Student student;
+
+        public Student SelectedStudent { get => student; set=>student = value; }
+
         public FrmEvaluation(Student selectedStudent) 
         {
             InitializeComponent();
@@ -39,6 +42,20 @@ namespace EvaluationManager {
 
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+
+            var evaluation = EvaluationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            if(evaluation!=null) 
+            {
+                txtTeacher.Text = evaluation.Evaluator.ToString();
+                txtEvaluationDate.Text = evaluation.EvaluationDate.ToString();
+                numPoints.Value = evaluation.Points;
+            }
+            else 
+            {
+                txtTeacher.Text = FrmLogin.LoggedTeacher.ToString();
+                txtEvaluationDate.Text = "-";
+                numPoints.Value = 0;
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e) 
