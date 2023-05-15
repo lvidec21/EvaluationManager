f4fd2a8949c96c7f9b9d65272538a27cb4df79f6
Luka Videc <lvidec21@foi.hr>
2023-05-15 17:08:31 +0200

    Dodana klasa Evaluation

--- EvaluationManager/EvaluationManager/FrmLogin.cs
+++ EvaluationManager/EvaluationManager/FrmLogin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EvaluationManager.Models;
+using EvaluationManager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,26 +12,30 @@ using System.Windows.Forms;
 
 namespace EvaluationManager
 {
-    public partial class FrmLogin : Form
-    {
-        public FrmLogin()
-        {
+    public partial class FrmLogin : Form {
+        public FrmLogin() {
             InitializeComponent();
         }
+        public static Teacher LoggedTeacher { get; set; }
+        {
 
+        }
         private void btnLogin_Click(object sender, EventArgs e) {
-            string username = txtUsername.Text;
-            string password = txtPassword.Text;
-
+            //string username = txtUsername.Text;
+            //string password = txtPassword.Text;
+            LoggedTeacher = TeacherRepository.GetTeacher(txtUsername.Text)
             if (username == "" || password == "") 
             {
                 MessageBox.Show("Popunite sva polja!", "Pogreška", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            } else if (username == "nastavnik" && password == "test") 
+            } 
+            else 
+            if (LoggedTeacher != null && LoggedTeacher.CheckPassword(txtPassword.Text)) 
             {
-                Hide();
-                FrmStudents frmStudents = new FrmStudents();
-                frmStudents.ShowDialog();
-                Close();
+       
+            FrmStudents frmStudents = new FrmStudents();
+            Hide();
+            frmStudents.ShowDialog();
+            Close();
             }
             else 
             {
